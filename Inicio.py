ad19a527f160e7a77db5385b8270cc59dea66831
Julissa <jn.valenzuela17@info.uas.edu.mx>
2021-09-20 11:45:51 -0600

    Introduccion a python

---
+++ Inicio.py
@@ -0,0 +1,23 @@
+#Impresiones
+print ("Hola mundo")
+
+#Variables
+x=6
+print(x)
+
+x=x+1
+print(x)
+
+#Condicionales
+if x<10:
+    print('Smaller')
+if x>20:
+    print('Bigger')
+print('Finish') 
+
+#Ciclos
+#1.-While
+while x<10:
+    print(x)
+    x=x+1
+print('OK!')
