178ca0ea04430af2aa4690fe40bc1641450874d2
Julissa <jn.valenzuela17@info.uas.edu.mx>
2021-09-23 13:17:48 -0600

    Operadores, conversion y concatenacion

---
+++ Operadores.py
@@ -0,0 +1,56 @@
+
+#Suma
+S=2
+S=S+2
+print('Suma')
+print(S)
+
+#Multiplicacion
+Mu=440*12
+print('Multiplicacion')
+print(Mu)
+
+#Division
+D=Mu/1000
+print('Division')
+print(D)
+
+#Modulo
+n=23
+Mo=n%5
+print('Modulo')
+print(Mo)
+
+#Potencia
+print('Potencia')
+print(4**3)
+
+
+#CONCATENACION----------------------------------------------------------------------------------------
+concate= 'Hello '+'there'
+print(concate)
+#concate=concate+ 1
+
+type(concate)               #type se usa para consultar el tipo de dato con el que trabajamos
+type(1)
+
+#CONVERSIONES----------------------------------------------------------------------------------------
+print(float(99)+100)
+print(int(42.0))
+
+sval='123'
+ival=int(sval)
+print(ival+1)
+
+#DIVISIONES ENTERAS-----------------------------------------------------------------------------------
+print(10/2)
+print(99/100)
+
+#ENTRADAS---------------------------------------------------------------------------------------------
+nombre=input('Quien eres? ')
+print('Bienvenido ', nombre)
+
+#RESUMEN ACTUAL---------------------------------------------------------------------------------------
+pisoE=input('Piso en Europa? ')
+pisoEUA=int(pisoE)+1
+print('El piso equivalente en EUA es ', pisoEUA)
